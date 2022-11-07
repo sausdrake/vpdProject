cb1be3f5a8c7c8339d19cd4263618502ef46fb28
temirlan <timaasanaliev03@gmail.com>
2022-11-07 23:57:38 +0700

    operation selection menu

--- practical.py
+++ practical.py
@@ -26,10 +26,27 @@ def subprogramThird(array_1, array_2, array_res = []):
         if elements_a not in array_2:
             array_res.append(elements_a)
     print(list(set(array_res)))
+
+def menu():
+    print("______________________Выберите операцию_____________________________\n"
+          "____________________________________________________________________\n"
+          "(1) Вывод элементов которые присутствуют в нескольких экземплярах\n"
+          "      либо только в массиве A, либо только в массиве В\n"
+          "____________________________________________________________________\n"
+          "(2) Вывод повторяющиеся элементов массива В, которые есть в массиве А\n"
+          "                  только в одном экземпляре\n"
+          "_____________________________________________________________________\n"
+          "(3) Вывод элементов, которые присутствуют в массиве А,\n"
+          "                 но отсутствуют в массиве В\n")
+    number_menu = input('number menu= ')
+    if number_menu == '1':
+        subprogramFirst(array_1, array_2)
+    elif number_menu == '2':
+        subprogramSecond(array_1, array_2)
+    elif number_menu == '3':
+        subprogramThird(array_1, array_2)
+
 array_1 = input('Введите массив А= ')
 array_2 = input('Введите массив В= ')
 array_1 = list(map(int, array_1.split()))
 array_2 = list(map(int, array_2.split()))
-#subprogramFirst(array_1, array_2)
-#subprogramSecond(array_1, array_2)
-subprogramThird(array_1, array_2)
