b884362905b33f09e8d29bd8204f3045a9530e43
temirlan <timaasanaliev03@gmail.com>
2022-11-07 23:29:48 +0700

    non-repeating elements in array A and the output of common elements with array B

--- practical.py
+++ practical.py
@@ -6,10 +6,22 @@ def subprogramFirst (array_1, array_2, array_res = []):
     print(array_res)
 def subprogramSecond (array_1, array_2, array_res = []):
     array_b_res = []
+    array_a_res = []
     while len(array_2) > 0:
-        print('array B_1= ', array_2)
         elements_b = array_2[0]
         array_2.remove(array_2[0])
         if (elements_b in array_2) and (elements_b not in array_b_res):
             array_b_res.append(elements_b)
-            print('array B= ', array_b_res)
+    for elements_a in array_1:
+        if array_1.count(elements_a) == 1:
+            array_a_res.append(elements_a)
+    for number_b in array_b_res:
+        if number_b in array_a_res:
+            array_res.append(number_b)
+    print(array_res)
+array_1 = input('Введите массив А= ')
+array_2 = input('Введите массив В= ')
+array_1 = list(map(int, array_1.split()))
+array_2 = list(map(int, array_2.split()))
+#subprogramFirst(array_1, array_2)
+subprogramSecond(array_1, array_2)
