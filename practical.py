2ebf35f8542f3c02cd92488fc1957ddb12e38f15
temirlan <timaasanaliev03@gmail.com>
2022-11-07 21:32:18 +0700

    duplicate array В elements

--- practical.py
+++ practical.py
@@ -4,3 +4,12 @@ def subprogramFirst (array_1, array_2, array_res = []):
         if number not in array_res:
             array_res.append(number)
     print(array_res)
+def subprogramSecond (array_1, array_2, array_res = []):
+    array_b_res = []
+    while len(array_2) > 0:
+        print('array B_1= ', array_2)
+        elements_b = array_2[0]
+        array_2.remove(array_2[0])
+        if (elements_b in array_2) and (elements_b not in array_b_res):
+            array_b_res.append(elements_b)
+            print('array B= ', array_b_res)
