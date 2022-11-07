cf80fa47f79d9f7d45cd8da22475ed6722dd462c
temirlan <timaasanaliev03@gmail.com>
2022-11-07 21:27:16 +0700

    first funaction(общие элементы двух массивов)

--- practical.py
+++ practical.py
@@ -1,2 +1,6 @@
-print('hello world')
-print(5+6)
+def subprogramFirst (array_1, array_2, array_res = []):
+    temp = array_1 + array_2
+    for number in temp:
+        if number not in array_res:
+            array_res.append(number)
+    print(array_res)
